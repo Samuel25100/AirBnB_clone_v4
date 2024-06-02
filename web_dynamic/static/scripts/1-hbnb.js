ce630b566e245c38a0c833d2ad0a115b74c107bc
Samuel25100 <samuelgselassie1@gmail.com>
2024-06-02 08:28:35 +0300

    add files for task 3

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,17 @@
+$(function() {
+  chkAmenity = {};
+  $.on('change', 'data-id', function() {
+    if (this.checked) {
+      chkAmenity[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete chkAmenity[$(this).data('id')];
+    }
+    let line = Object.values(chkAmenity);
+    if (line.length > 0) {
+      $('div.amenities > h4').text(Object.values(chkAmenity));
+    } else {
+      $('div.amenities > h4').html('&nbsp;');
+    }
+    console.log(chkAmenity);
+  })
+});
